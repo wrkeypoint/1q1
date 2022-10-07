57e060ffa596990a7f253e4a8c4f3b04ba75c6b9
lin <lin@2.com>
2022-10-07 15:20:49 +0800

    master commit3

--- src/main/java/git/a.java
+++ src/main/java/git/a.java
@@ -6,8 +6,6 @@ public class a {
         System.out.println("github1");
         System.out.println("hotfix");
         System.out.println("hotfix1");
-        System.out.println("11");
-
-        System.out.println("12");
+        System.out.println("push test");
     }
 }
